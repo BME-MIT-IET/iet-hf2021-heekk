5489bce8ef71d11d99b4218f783d05155fadfe3a
rhalm <49129049+rhalm@users.noreply.github.com>
2021-05-08 20:37:15 +0200

    minified, reran tests, fixed issues #1

--- url.js
+++ url.js
@@ -8,10 +8,10 @@
  * @license MIT
  * @author Mykhailo Stadnyk <mikhus@gmail.com> and contributors
  * @see https://github.com/Mikhus/domurl/graphs/contributors
- */
-'use strict';
+*/
+"use strict";
 
-const ns = (typeof module !== 'undefined' && module.exports ? module : window)    
+const ns = (typeof module !== "undefined" && module.exports ? module : window);
 
 var RX_PROTOCOL = /^[a-z]+:/;
 var RX_PORT = /[-a-z0-9]+(\.[-a-z0-9])*:\d+/i;
@@ -33,7 +33,7 @@ var RX_PATH_SEMI = /^\w:$/;
 var RX_URL_TEST = /[^/#?]/;
 
 // configure given url options
-function urlConfig (url) {
+function urlConfig(url) {
     var config = {
         path: true,
         query: true,
@@ -61,23 +61,20 @@ function urlConfig (url) {
     return config;
 }
 
-var isNode = typeof window === 'undefined' &&
-    typeof global !== 'undefined' &&
-    typeof require === 'function';
-var isIe = !isNode && ns.navigator && ns.navigator.userAgent &&
-    ~ns.navigator.userAgent.indexOf('MSIE');
+var isNode = typeof window === "undefined" && typeof global !== "undefined" && typeof require === "function";
+var isIe = !isNode && ns.navigator && ns.navigator.userAgent && ~ns.navigator.userAgent.indexOf("MSIE");
 
 // Trick to bypass Webpack's require at compile time
-var nodeRequire = isNode ? ns['require'] : null;
+var nodeRequire = isNode ? ns["require"] : null;
 
 // mapping between what we want and <a> element properties
 var map = {
-    protocol: 'protocol',
-    host: 'hostname',
-    port: 'port',
-    path: 'pathname',
-    query: 'search',
-    hash: 'hash'
+    protocol: "protocol",
+    host: "hostname",
+    port: "port",
+    path: "pathname",
+    query: "search",
+    hash: "hash"
 };
 
 // jscs: disable
@@ -100,26 +97,28 @@ var _currNodeUrl;
 function getCurrUrl() {
     if (isNode) {
         if (!_currNodeUrl) {
-            _currNodeUrl = ('file://' +
-                (process.platform.match(RX_WIN) ? '/' : '') +
-                nodeRequire('fs').realpathSync('.')
-            );
+            _currNodeUrl =
+                "file://" + (process.platform.match(RX_WIN) ? "/" : "") + nodeRequire("fs").realpathSync(".");
         }
         return _currNodeUrl;
-    } else if (document.location.href === 'about:srcdoc') {
+    } else if (document.location.href === "about:srcdoc") {
         return self.parent.document.location.href;
     } else {
         return document.location.href;
     }
 }
 
-function parse (self, url, absolutize) {
+function parse(self, url, absolutize) {
     var link, i, auth;
 
-    if (!url)  url = getCurrUrl();
-    if (isNode) link = nodeRequire('url').parse(url);
-    else {
-        link = document.createElement('a');
+    if (!url) {
+        url = getCurrUrl();
+    }
+
+    if (isNode) {
+        link = nodeRequire("url").parse(url);
+    } else {
+        link = document.createElement("a");
         link.href = url;
     }
 
@@ -128,69 +127,74 @@ function parse (self, url, absolutize) {
     auth = url.match(RX_CREDS) || [];
 
     for (i in map) {
-        if (config[i]) self[i] = link[map[i]] || '';
-        else self[i] = '';
+        if (config[i]) {
+            self[i] = link[map[i]] || "";
+        } else {
+            self[i] = "";
+        }
     }
 
     // fix-up some parts
-    self.protocol = self.protocol.replace(RX_PROTOCOL_REPL, '');
-    self.query = self.query.replace(RX_QUERY_REPL, '');
-    self.hash = decode(self.hash.replace(RX_HASH_REPL, ''));
-    self.user = decode(auth[1] || '');
-    self.pass = decode(auth[2] || '');
+    self.protocol = self.protocol.replace(RX_PROTOCOL_REPL, "");
+    self.query = self.query.replace(RX_QUERY_REPL, "");
+    self.hash = decode(self.hash.replace(RX_HASH_REPL, ""));
+    self.user = decode(auth[1] || "");
+    self.pass = decode(auth[2] || "");
     /* jshint ignore:start */
-    self.port = (
+    self.port =
         // loosely compare because port can be a string
         defaultPorts[self.protocol] == self.port || self.port == 0
-    ) ? '' : self.port; // IE fix, Android browser fix
+            ? ""
+            : self.port; // IE fix, Android browser fix
     /* jshint ignore:end */
 
-    if (!config.protocol && RX_URL_TEST.test(url.charAt(0)))
-        self.path = url.split('?')[0].split('#')[0];
+    if (!config.protocol && RX_URL_TEST.test(url.charAt(0))) {
+        self.path = url.split("?")[0].split("#")[0];
+    }
 
-    self.path = updatePath(self, config.protocol, absolutize)
+    self = updatePath(self, config.protocol, absolutize, url);
+    
 
     self.paths(self.paths());
 
     self.query = new QueryString(self.query);
 }
 
-function updatePath(self, protocol, absolutize) {
-    if (!protocol && absolutize) { // if it's IE and path is relative
-            
+function updatePath(self, protocol, absolutize, url) {
+    if (!protocol && absolutize) { // is IE and path is relative
         var base = new Url(getCurrUrl().match(RX_PATH)[0]);
         var basePath = base.path.split("/");
         var selfPath = self.path.split("/");
         var props = ["protocol", "user", "pass", "host", "port"];
-        var s = props.length;
 
         basePath.pop();
 
-        for (let i = 0; i < s; i++) self[props[i]] = base[props[i]];
+        for (let i = 0; i < props.length; i++) {
+            self[props[i]] = base[props[i]];
+        }
 
         while (selfPath[0] === "..") {
             // skip all "../
             basePath.pop();
             selfPath.shift();
         }
+
         self.path = (url.charAt(0) !== "/" ? basePath.join("/") : "") + "/" + selfPath.join("/");
     }
 
     self.path = self.path.replace(RX_PATH_FIX, "/");
     isIe && (self.path = self.path.replace(RX_PATH_IE_FIX, "/"));
-
-    return self.path;
+    return self;
 }
 
-
-function encode (s) {
-    return encodeURIComponent(s).replace(RX_SINGLE_QUOTE, '%27');
+function encode(s) {
+    return encodeURIComponent(s).replace(RX_SINGLE_QUOTE, "%27");
 }
 
-function decode (s) {
-    s = s.replace(RX_PLUS, ' ');
-    s = s.replace(RX_DECODE_1, function (code, hex1, hex2, hex3) {
-        var n1 = parseInt(hex1, 16) - 0xE0;
+function decode(s) {
+    s = s.replace(RX_PLUS, " ");
+    s = s.replace(RX_DECODE_1, function(code, hex1, hex2, hex3) {
+        var n1 = parseInt(hex1, 16) - 0xe0;
         var n2 = parseInt(hex2, 16) - 0x80;
 
         if (n1 === 0 && n2 < 32) {
@@ -200,14 +204,14 @@ function decode (s) {
         var n3 = parseInt(hex3, 16) - 0x80;
         var n = (n1 << 12) + (n2 << 6) + n3;
 
-        if (n > 0xFFFF) {
+        if (n > 0xffff) {
             return code;
         }
 
         return String.fromCharCode(n);
     });
-    s = s.replace(RX_DECODE_2, function (code, hex1, hex2) {
-        var n1 = parseInt(hex1, 16) - 0xC0;
+    s = s.replace(RX_DECODE_2, function(code, hex1, hex2) {
+        var n1 = parseInt(hex1, 16) - 0xc0;
 
         if (n1 < 2) {
             return code;
@@ -218,7 +222,7 @@ function decode (s) {
         return String.fromCharCode((n1 << 6) + n2);
     });
 
-    return s.replace(RX_DECODE_3, function (code, hex) {
+    return s.replace(RX_DECODE_3, function(code, hex) {
         return String.fromCharCode(parseInt(hex, 16));
     });
 }
@@ -229,12 +233,12 @@ function decode (s) {
  * @param {string} qs - string representation of QueryString
  * @constructor
  */
-function QueryString (qs) {
-    var parts = qs.split('&');
+function QueryString(qs) {
+    var parts = qs.split("&");
 
     for (var i = 0, s = parts.length; i < s; i++) {
-        var keyVal = parts[i].split('=');
-        var key = decodeURIComponent(keyVal[0].replace(RX_PLUS, ' '));
+        var keyVal = parts[i].split("=");
+        var key = decodeURIComponent(keyVal[0].replace(RX_PLUS, " "));
 
         if (!key) {
             continue;
@@ -259,8 +263,8 @@ function QueryString (qs) {
  *
  * @returns {string}
  */
-QueryString.prototype.toString = function () {
-    var s = '';
+QueryString.prototype.toString = function() {
+    var s = "";
     var e = encode;
     var i, ii;
 
@@ -277,7 +281,7 @@ QueryString.prototype.toString = function () {
             if (!len) {
                 // Parameter is an empty array, so treat as
                 // an empty argument
-                s += (s ? '&' : '') + e(i) + '=';
+                s += (s ? "&" : "") + e(i) + "=";
                 continue;
             }
 
@@ -286,17 +290,15 @@ QueryString.prototype.toString = function () {
                 if (v === undefined) {
                     continue;
                 }
-                s += s ? '&' : '';
-                s += e(i) + (v === null
-                    ? ''
-                    : '=' + e(v));
+                s += s ? "&" : "";
+                s += e(i) + (v === null ? "" : "=" + e(v));
             }
             continue;
         }
 
         // Plain value
-        s += s ? '&' : '';
-        s += e(i) + (w === null ? '' : '=' + e(w));
+        s += s ? "&" : "";
+        s += e(i) + (w === null ? "" : "=" + e(w));
     }
 
     return s;
@@ -309,7 +311,7 @@ QueryString.prototype.toString = function () {
  * @param {boolean} [noTransform] - do not transform to absolute URL
  * @constructor
  */
-function Url (url, noTransform) {
+function Url(url, noTransform) {
     parse(this, url, !noTransform);
 }
 
@@ -318,7 +320,7 @@ function Url (url, noTransform) {
  *
  * @returns {Url}
  */
-Url.prototype.clearQuery = function () {
+Url.prototype.clearQuery = function() {
     for (var key in this.query) {
         if (!(this.query[key] instanceof Function)) {
             delete this.query[key];
@@ -333,7 +335,7 @@ Url.prototype.clearQuery = function () {
  *
  * @returns {number}
  */
-Url.prototype.queryLength = function () {
+Url.prototype.queryLength = function() {
     var count = 0;
 
     for (var key in this.query) {
@@ -350,7 +352,7 @@ Url.prototype.queryLength = function () {
  *
  * @returns {boolean}
  */
-Url.prototype.isEmptyQuery = function () {
+Url.prototype.isEmptyQuery = function() {
     return this.queryLength() === 0;
 };
 
@@ -360,27 +362,24 @@ Url.prototype.isEmptyQuery = function () {
  *                          Url.path property
  * @returns {Array} - an array representation of the Url.path property
  */
-Url.prototype.paths = function (paths) {
-    var prefix = '';
+Url.prototype.paths = function(paths) {
+    var prefix = "";
     var i = 0;
     var s;
 
-    if (paths && paths.length && paths + '' != paths) {
+    if (paths && paths.length && paths + "" !== paths) {
         if (this.isAbsolute()) {
-            prefix = '/';
+            prefix = "/";
         }
 
         for (s = paths.length; i < s; i++) {
-            paths[i] = !i && RX_PATH_SEMI.test(paths[i])
-                ? paths[i]
-                : encode(paths[i]);
+            paths[i] = !i && RX_PATH_SEMI.test(paths[i]) ? paths[i] : encode(paths[i]);
         }
 
-        this.path = prefix + paths.join('/');
+        this.path = prefix + paths.join("/");
     }
 
-    paths = (this.path.charAt(0) === '/' ?
-        this.path.slice(1) : this.path).split('/');
+    paths = (this.path.charAt(0) === "/" ? this.path.slice(1) : this.path).split("/");
 
     for (i = 0, s = paths.length; i < s; i++) {
         paths[i] = decode(paths[i]);
@@ -413,8 +412,8 @@ Url.prototype.decode = decode;
  *
  * @returns {boolean}
  */
-Url.prototype.isAbsolute = function () {
-    return this.protocol || this.path.charAt(0) === '/';
+Url.prototype.isAbsolute = function() {
+    return this.protocol || this.path.charAt(0) === "/";
 };
 
 /**
@@ -422,18 +421,16 @@ Url.prototype.isAbsolute = function () {
  *
  * @returns {string}
  */
-Url.prototype.toString = function () {
+Url.prototype.toString = function() {
     return (
-        (this.protocol && (this.protocol + '://')) +
-        (this.user && (
-            encode(this.user) + (this.pass && (':' + encode(this.pass))
-        ) + '@')) +
-        (this.host) +
-        (this.port && (':' + this.port)) +
-        (this.path) +
-        (this.query.toString() && ('?' + this.query)) +
-        (this.hash && ('#' + encode(this.hash)))
+        (this.protocol && this.protocol + "://") +
+        (this.user && encode(this.user) + (this.pass && ":" + encode(this.pass)) + "@") +
+        (this.host && this.host) +
+        (this.port && ":" + this.port) +
+        (this.path && this.path) +
+        (this.query.toString() && "?" + this.query) +
+        (this.hash && "#" + encode(this.hash))
     );
 };
 
-ns[ns.exports ? 'exports' : 'Url'] = Url;
+ns[ns.exports ? "exports" : "Url"] = Url;
